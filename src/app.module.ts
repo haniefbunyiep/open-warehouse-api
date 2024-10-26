a4da3c3427750fbf8a14012e63e752d22b6c0c2b
Hanief Burhanuddin <haniefburhan17@gmail.com>
2024-10-26 12:03:52 +0700

    chore: change flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
